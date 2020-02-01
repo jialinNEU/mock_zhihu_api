d5cb30201ad4ee477e230697d62b1f993adec2ab
Jialin <jgao2306@163.com>
2020-02-02 01:23:38 +0800

    feat: add topic page pagination and blur search

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,8 +1,15 @@
 const Topic = require('../models/topics');
 
 class TopicsController {
+  // 具备分页功能
   async find(ctx) {
-    ctx.body = await Topic.find();
+    const { per_page = 10 } = ctx.query;
+    const page = Math.max(ctx.query.page * 1, 1) - 1;
+    const perPage = Math.max(per_page * 1, 1);
+    ctx.body = await Topic
+                .find({ name: new RegExp(ctx.query.q) })
+                .limit(perPage) // 限制每页数量
+                .skip(page * perPage); // 跳过前n项，第一页跳过前 0 项
   }
 
   async findById(ctx) {
